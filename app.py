818b88602c15e2cd99af58caabbfed1a1d6c6e92
Shruti Saxena <ShrutiSaxena046@gmail.com>
2026-09-06 21:46:21 +0530

    Remove icons and emojis from interface

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import streamlit as st
 # PAGE CONFIGURATION
 
 st.set_page_config(
-    page_title="Email Spam Detector",
-    page_icon="📧",
+    page_title="Email Spam Detector",,
     layout="wide"
 )
 
